7b1a9fb0d8a49b6c85285cb4b73b2bb0ba4eeb9a
Roman <vilensky@gmail.com>
2019-03-16 22:31:28 -0700

    actuator

--- src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,18 @@ package com.in28minutes.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ApplicationContext appCtx = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+//		for (String beanName : appCtx.getBeanDefinitionNames()) {
+//
+//			System.out.println("BEAN: " + beanName);
+//		}
 	}
 
 }
